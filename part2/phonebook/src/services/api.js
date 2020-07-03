bc11d9e0a2e190f5bdf990bf417c983559c5393e
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-07-03 09:34:59 +0200

    Add phonebook part2 submission

--- part2/phonebook/src/services/api.js
+++ part2/phonebook/src/services/api.js
@@ -3,5 +3,23 @@ import axios from 'axios';
 const baseURL = "http://localhost:3001/persons"
 
 
-export default baseURL;
+const getAll = () => {
+  const request = axios.get(baseURL)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseURL, newObject);
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseURL}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+const remove = id => {
+  return axios.delete(`${baseURL}/${id}`)
+}
+
+export { getAll, create, update, remove };
 
