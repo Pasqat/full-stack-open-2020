046b92710ec7ebfb0450bc544fac699f59ad4b3a
Pasqat <pasquale.matarrese@gmail.com>
2020-07-04 12:29:46 +0200

    part3

--- part3/phonebook/src/services/api.js
+++ part3/phonebook/src/services/api.js
@@ -1,25 +1,7 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:3001/persons"
+const baseURL = "/api/persons"
 
 
-const getAll = () => {
-  const request = axios.get(baseURL)
-  return request.then(response => response.data)
-}
-
-const create = newObject => {
-  const request = axios.post(baseURL, newObject);
-  return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseURL}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-const remove = id => {
-  return axios.delete(`${baseURL}/${id}`)
-}
-
-export { getAll, create, update, remove };
+export default baseURL;
 
