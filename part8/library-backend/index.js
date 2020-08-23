a55a96a698601a9adafdc6e00691e0683430f4b9
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-08-23 15:50:29 +0200

    part8.2 allBooks query

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,6 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `
 
@@ -104,6 +105,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
