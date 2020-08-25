05c5a1aa751d90ad4deecc1af6f8609524a07608
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-08-25 15:42:33 +0200

    part8.7 add editAuthor mutation

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -109,7 +109,11 @@ const typeDefs = gql`
       author: String!,
       published: Int,
       genres: [String]!
-    ) : Books
+    ): Books
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -151,6 +155,16 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
