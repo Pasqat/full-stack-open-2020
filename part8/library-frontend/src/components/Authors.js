9b5261997c379f7161163836538243fe9babaf08
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-08-26 11:03:36 +0200

    part8.11 add editAuthor view and mutation

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,8 +1,50 @@
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-import {useQuery} from '@apollo/client'
-import {ALL_AUTHORS} from '../queries'
+import {useQuery, useMutation} from '@apollo/client'
+import {ALL_AUTHORS, EDIT_AUTHOR} from '../queries'
+
+const SetBirthyear = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [editAuthor, result] = useMutation(EDIT_AUTHOR)
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      alert('Author not found')
+    }
+  }, [result.data]) // eslint-disable-line
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    editAuthor({variables: {name, setBornTo: Number(born)}})
+
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h3>Set Birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name: <input type="text"
+            value={name}
+            onChange={({target}) => setName(target.value)} />
+        </div>
+        <div>
+          born: <input type="text"
+            value={born}
+            onChange={({target}) => setBorn(target.value)} />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
@@ -11,6 +53,7 @@ const Authors = (props) => {
     return null
   }
 
+  const authors = []
   if (result.loading) {
     return <div> Loading... </div>
   }
@@ -38,7 +81,7 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <SetBirthyear />
     </div>
   )
 }
--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 import {useMutation} from '@apollo/client'
-import {CREATE_BOOK, ALL_BOOKS} from '../queries'
+import {CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS} from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -11,7 +11,7 @@ const NewBook = (props) => {
   const [genres, setGenres] = useState([])
 
   const [createBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{query: ALL_BOOKS}]
+    refetchQueries: [{query: ALL_BOOKS, ALL_AUTHORS}]
   })
 
   if (!props.show) {
