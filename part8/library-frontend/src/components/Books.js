ec4498f5665b4e2ca5bee667840fc8f236e48eb1
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-09-01 18:31:47 +0200

    part8.19 books by genre, part 1 (from react)

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {useQuery} from '@apollo/client'
 import {ALL_BOOKS} from '../queries'
 
 const Books = (props) => {
+
+  const [genre, setGenre] = useState(null)
+
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -13,6 +16,20 @@ const Books = (props) => {
   if (result.loading) {
     return <div>Loading...</div>
   }
+
+  const bookFiltered = result.data.allBooks.filter(book => book.genres.includes(genre))
+
+  // Generate the array of genres without duplicates
+  let jointArray = []
+  const booksArray = result.data.allBooks.map(book => book.genres)
+  booksArray.forEach(array => {
+    jointArray = [...jointArray, ...array]
+  })
+  const genresList = [...new Set([...jointArray])]
+
+  const booksView = () =>
+    !genre ? result.data.allBooks : bookFiltered
+
 
   return (
     <div>
@@ -29,7 +46,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {result.data.allBooks.map(a =>
+          {booksView().map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -38,6 +55,8 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {genresList.map(g => <button key={g} onClick={() => setGenre(g)}>{g}</button>)}
+      <button onClick={() => setGenre(null)}>all genre</button>
     </div>
   )
 }
