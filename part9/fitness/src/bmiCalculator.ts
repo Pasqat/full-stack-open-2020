1f8f4a2d66b1723d133a56a8f2a84ad7d301cb2f
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-09-16 10:49:51 +0200

    part9.1 body mass index

---
+++ part9/fitness/src/bmiCalculator.ts
@@ -0,0 +1,18 @@
+function calculateBMI(height: number, weight: number): string {
+  const BMI = weight / ((height / 100) ** 2);
+
+  if (BMI <= 15) {
+    return "Very severly underweight";
+  } else if (BMI > 15 && BMI <= 16) {
+    return "Severely underweight";
+  } else if (BMI > 16 && BMI <= 18.5) {
+    return "Underweight";
+  } else if (BMI > 18.5 && BMI <= 25) {
+    return "Normal (healthy weight)"
+  } else if (BMI > 25 && BMI <= 30) {
+    return " Overweight"
+  } else {
+    return "Obese"
+  }
+}
+console.log(calculateBMI(180, 74))
